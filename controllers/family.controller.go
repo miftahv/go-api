e9ebd26af6683da247da054b9e960d8549d7b01f
Miftah Fadhlurrohman <48742336+miftahv@users.noreply.github.com>
2024-01-10 12:43:22 +0700

    Update family.controller.go

--- controllers/family.controller.go
+++ controllers/family.controller.go
@@ -16,3 +16,18 @@ func FetchAllFamily(c echo.Context) error{
 
 	return c.JSON(http.StatusOK, result)
 }
+func FetchFamilyById(c echo.Context) error{
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"message": "Invalid ID"})
+	}
+	result, err := models.FetchFamilyById(id)
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()} )
+	}
+
+	return c.JSON(http.StatusOK, result)
+	
+
+}
